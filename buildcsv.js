f17e3c2828e67a343b4c63a157df4d203e3dfbea
Keith Woodward <keith@everdays.com>
2022-07-27 19:02:34 -0400

    split out csv function so i can use it for just one doc, add parsing of betting info from roto

--- buildcsv.js
+++ buildcsv.js
@@ -1,25 +1,31 @@
 import converter from 'json-2-csv';
 import * as fs from 'fs';
 
-export default async (gamelogs) => { //Takes in an object containing gamelogs for "hitters" and "pitchers"
+const makeCsv = async (inputData, name) => {
+    await converter.json2csv(inputData, (err, csv) => { //Convert their objects to csv
+        if (err) {
+            throw err;
+        }
+
+        const writeStream = fs.createWriteStream(`${name}.csv`);
+        writeStream.write(csv); //Write it to a file with this name ^
+
+        writeStream.on('finish', () => {
+            console.log(`done writing to csv: ${name}`);
+        });
+
+        writeStream.on('error', (err) => {
+            console.error(err);
+        });
+
+        writeStream.end();
+    })
+};
+
+const baseballGamelogCsv = async (gamelogs) => { //Takes in an object containing gamelogs for "hitters" and "pitchers"
     for (const gamelogsByType of Object.keys(gamelogs)) { //For both of those...
-        converter.json2csv(gamelogs[gamelogsByType], (err, csv) => { //Convert their objects to csv
-            if (err) {
-                throw err;
-            }
-
-            const writeStream = fs.createWriteStream(`gamelog-${gamelogsByType}.csv`);
-            writeStream.write(csv); //Write it to a file with this name ^
-
-            writeStream.on('finish', () => {
-                console.log(`done writing to csv for ${gamelogsByType}s`);
-            });
-
-            writeStream.on('error', (err) => {
-                console.error(err);
-            });
-
-            writeStream.end();
-        })
+        await makeCsv(gamelogs[gamelogsByType], `gamelog-${gamelogsByType}`)
     }
 };
+
+export { baseballGamelogCsv, makeCsv };
